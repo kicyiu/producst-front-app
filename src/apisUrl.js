3170d6066b770162e92dec8ab6c6d34e02f74227
kicyiu <albertokicyiu@gmail.com>
2020-09-15 17:07:37 -0300

    cambios menores

--- src/apisUrl.js
+++ src/apisUrl.js
@@ -1,5 +1,4 @@
+console.log('API URL: ', process.env.NODE_ENV);
 
-console.log('Enviroment: ', process.env.ENVIROMENT);
-
-export const findProductsUrl = process.env.ENVIROMENT === 'production' ? "https://ky-products-api.herokuapp.com/api/products/findProducts" : "http://localhost:8080/api/products/findProducts" ;
+export const findProductsUrl = `${process.env.API_URL}/findProducts`;
 
