a5fa63e2e1a793504271ed68a36d0114f1919da8
kicyiu <albertokicyiu@gmail.com>
2020-09-15 16:27:38 -0300

    set enviroment var

--- src/apisUrl.js
+++ src/apisUrl.js
@@ -1,3 +1,5 @@
 
+console.log('Enviroment: ', process.env.ENVIROMENT);
+
 export const findProductsUrl = process.env.ENVIROMENT === 'production' ? "https://ky-products-api.herokuapp.com/api/products/findProducts" : "http://localhost:8080/api/products/findProducts" ;
 
