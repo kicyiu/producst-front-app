30c094696a16c9de92fc912b903f97705fad166e
kicyiu <albertokicyiu@gmail.com>
2020-09-15 17:18:41 -0300

    cambio variables de entorno

--- src/apisUrl.js
+++ src/apisUrl.js
@@ -1,4 +1,6 @@
 console.log('API URL: ', process.env.NODE_ENV);
 
-export const findProductsUrl = `${process.env.API_URL}/findProducts`;
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://ky-products-api.herokuapp.com/api' : 'http://localhost:8080/api'; 
+
+export const findProductsUrl =  `${API_URL}/products/findProducts`;
 
